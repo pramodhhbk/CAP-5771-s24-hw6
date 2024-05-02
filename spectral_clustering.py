7a728f2a63ef329fc35a53cc36551e7a446f9027
Pramodh Sairam <45509400+pramodhhbk@users.noreply.github.com>
2024-05-01 21:46:04 -0400

    Update spectral_clustering.py

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -7,9 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
 import pickle
-from scipy.linalg import eigh
 from scipy.cluster.vq import kmeans2
-import matplotlib.backends.backend_pdf as pdf
+from scipy.linalg import eigh
 from typing import Tuple,Optional
 
 ######################################################################
